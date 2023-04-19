7fc941df38cf3ab62eb965f34adb0a00ac76ff2b
Stepan Kudlacek <stepanek.kudlacek@gmail.com>
2023-04-19 09:18:52 +0200

    nearly done

---
+++ cviko8/cmake-build-debug/CMakeFiles/cviko8.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/stepankudlacek/Desktop/AL/cviko8/BST.cpp" "CMakeFiles/cviko8.dir/BST.cpp.o" "gcc" "CMakeFiles/cviko8.dir/BST.cpp.o.d"
+  "/Users/stepankudlacek/Desktop/AL/cviko8/BST_string.cpp" "CMakeFiles/cviko8.dir/BST_string.cpp.o" "gcc" "CMakeFiles/cviko8.dir/BST_string.cpp.o.d"
+  "/Users/stepankudlacek/Desktop/AL/cviko8/main.cpp" "CMakeFiles/cviko8.dir/main.cpp.o" "gcc" "CMakeFiles/cviko8.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cviko8/cmake-build-debug/CMakeFiles/cviko8.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cviko8.dir/BST.cpp.o"
+  "CMakeFiles/cviko8.dir/BST.cpp.o.d"
+  "CMakeFiles/cviko8.dir/BST_string.cpp.o"
+  "CMakeFiles/cviko8.dir/BST_string.cpp.o.d"
+  "CMakeFiles/cviko8.dir/main.cpp.o"
+  "CMakeFiles/cviko8.dir/main.cpp.o.d"
+  "cviko8"
+  "cviko8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cviko8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
